844e730eeefb10c392e360de580198e7b4f0bee6
FIRST_NAME LAST_NAMEjaisal1311 <jaisalashah123@gmail.com>
2021-05-06 13:35:05 +0530

    context api init

--- amazon-clone/src/components/Product.js
+++ amazon-clone/src/components/Product.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { useStateValue } from "../stateProvider";
 import range from "lodash/range";
 
 import "./Product.css";
 
-const Product = ({ title, image, price, rating }) => {
+const Product = ({ id, title, image, price, rating }) => {
+  const [state, dispatch] = useStateValue();
+  console.log(state.basket);
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+    console.log("sdfvs >>", state);
+  };
   const star_rating = range(rating);
-  console.log(star_rating);
   return (
     <div className="product">
       <div className="product__info">
@@ -21,7 +36,13 @@ const Product = ({ title, image, price, rating }) => {
         </div>
       </div>
       <img src={image} alt="" />
-      <button>Add to Basket</button>
+      <button
+        onClick={() => {
+          addToBasket();
+        }}
+      >
+        Add to Basket
+      </button>
     </div>
   );
 };
